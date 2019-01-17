46d98b996aa4622326e2d1c95d2f1ee76eff8a59
crosstimefish <crosstimefish@gamil.com>
2019-01-17 15:48:24 +0800

    refine code

--- TerrainMap/TerrainMapGUILibrary/Extensions/ControlExtension.cs
+++ TerrainMap/TerrainMapGUILibrary/Extensions/ControlExtension.cs
@@ -18,8 +18,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public override bool AllowDrop
         {
-            get { return base.AllowDrop; }
-            set { base.AllowDrop = value; }
+            get
+            {
+                return base.AllowDrop;
+            }
+            set
+            {
+                base.AllowDrop = value;
+            }
         }
 
         [Browsable(false)]
@@ -27,8 +33,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public override Image BackgroundImage
         {
-            get { return base.BackgroundImage; }
-            set { base.BackgroundImage = value; }
+            get
+            {
+                return base.BackgroundImage;
+            }
+            set
+            {
+                base.BackgroundImage = value;
+            }
         }
 
         [Browsable(false)]
@@ -36,8 +48,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public override ImageLayout BackgroundImageLayout
         {
-            get { return base.BackgroundImageLayout; }
-            set { base.BackgroundImageLayout = value; }
+            get
+            {
+                return base.BackgroundImageLayout;
+            }
+            set
+            {
+                base.BackgroundImageLayout = value;
+            }
         }
 
         [Browsable(false)]
@@ -45,8 +63,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public override ContextMenuStrip ContextMenuStrip
         {
-            get { return base.ContextMenuStrip; }
-            set { base.ContextMenuStrip = value; }
+            get
+            {
+                return base.ContextMenuStrip;
+            }
+            set
+            {
+                base.ContextMenuStrip = value;
+            }
         }
 
         [Browsable(false)]
@@ -54,30 +78,54 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public override string Text
         {
-            get { return base.Text; }
-            set { base.Text = value; }
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+                base.Text = value;
+            }
         }
 
         [DefaultValue(typeof(Color), "White")]
         public override Color BackColor
         {
-            get { return base.BackColor; }
-            set { base.BackColor = value; }
+            get
+            {
+                return base.BackColor;
+            }
+            set
+            {
+                base.BackColor = value;
+            }
         }
 
         [DefaultValue(typeof(Font), FontTheme.NormalString)]
         public override Font Font
         {
-            get { return base.Font; }
-            set { base.Font = value; }
+            get
+            {
+                return base.Font;
+            }
+            set
+            {
+                base.Font = value;
+            }
         }
 
         [Browsable(false)]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new bool CausesValidation
         {
-            get { return base.CausesValidation; }
-            set { base.CausesValidation = value; }
+            get
+            {
+                return base.CausesValidation;
+            }
+            set
+            {
+                base.CausesValidation = value;
+            }
         }
 
         [DefaultValue(false)]
@@ -86,8 +134,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new bool TabStop
         {
-            get { return false; }
-            set { base.TabStop = false; }
+            get
+            {
+                return false;
+            }
+            set
+            {
+                base.TabStop = false;
+            }
         }
 
         [DefaultValue(0)]
@@ -96,15 +150,27 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new int TabIndex
         {
-            get { return 0; }
-            set { base.TabIndex = 0; }
+            get
+            {
+                return 0;
+            }
+            set
+            {
+                base.TabIndex = 0;
+            }
         }
 
         [DefaultValue(typeof(Size), "100, 100")]
         public new Size Size
         {
-            get { return base.Size; }
-            set { base.Size = value; }
+            get
+            {
+                return base.Size;
+            }
+            set
+            {
+                base.Size = value;
+            }
         }
 
         [Browsable(false)]
@@ -112,8 +178,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler BackgroundImageChanged
         {
-            add { base.BackgroundImageChanged += value; }
-            remove { base.BackgroundImageChanged -= value; }
+            add
+            {
+                base.BackgroundImageChanged += value;
+            }
+            remove
+            {
+                base.BackgroundImageChanged -= value;
+            }
         }
 
         [Browsable(false)]
@@ -121,8 +193,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler BackgroundImageLayoutChanged
         {
-            add { base.BackgroundImageLayoutChanged += value; }
-            remove { base.BackgroundImageLayoutChanged -= value; }
+            add
+            {
+                base.BackgroundImageLayoutChanged += value;
+            }
+            remove
+            {
+                base.BackgroundImageLayoutChanged -= value;
+            }
         }
 
         [Browsable(false)]
@@ -130,8 +208,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler CausesValidationChanged
         {
-            add { base.CausesValidationChanged += value; }
-            remove { base.CausesValidationChanged -= value; }
+            add
+            {
+                base.CausesValidationChanged += value;
+            }
+            remove
+            {
+                base.CausesValidationChanged -= value;
+            }
         }
 
         [Browsable(false)]
@@ -139,8 +223,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler ContextMenuStripChanged
         {
-            add { base.ContextMenuStripChanged += value; }
-            remove { base.ContextMenuStripChanged -= value; }
+            add
+            {
+                base.ContextMenuStripChanged += value;
+            }
+            remove
+            {
+                base.ContextMenuStripChanged -= value;
+            }
         }
 
         [Browsable(false)]
@@ -148,8 +238,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event DragEventHandler DragDrop
         {
-            add { base.DragDrop += value; }
-            remove { base.DragDrop -= value; }
+            add
+            {
+                base.DragDrop += value;
+            }
+            remove
+            {
+                base.DragDrop -= value;
+            }
         }
 
         [Browsable(false)]
@@ -157,8 +253,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event DragEventHandler DragEnter
         {
-            add { base.DragEnter += value; }
-            remove { base.DragEnter -= value; }
+            add
+            {
+                base.DragEnter += value;
+            }
+            remove
+            {
+                base.DragEnter -= value;
+            }
         }
 
         [Browsable(false)]
@@ -166,8 +268,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler DragLeave
         {
-            add { base.DragLeave += value; }
-            remove { base.DragLeave -= value; }
+            add
+            {
+                base.DragLeave += value;
+            }
+            remove
+            {
+                base.DragLeave -= value;
+            }
         }
 
         [Browsable(false)]
@@ -175,8 +283,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event DragEventHandler DragOver
         {
-            add { base.DragOver += value; }
-            remove { base.DragOver -= value; }
+            add
+            {
+                base.DragOver += value;
+            }
+            remove
+            {
+                base.DragOver -= value;
+            }
         }
 
         [Browsable(false)]
@@ -184,8 +298,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler TabStopChanged
         {
-            add { base.TabStopChanged += value; }
-            remove { base.TabStopChanged -= value; }
+            add
+            {
+                base.TabStopChanged += value;
+            }
+            remove
+            {
+                base.TabStopChanged -= value;
+            }
         }
 
         [Browsable(false)]
@@ -193,8 +313,14 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler TabIndexChanged
         {
-            add { base.TabIndexChanged += value; }
-            remove { base.TabIndexChanged -= value; }
+            add
+            {
+                base.TabIndexChanged += value;
+            }
+            remove
+            {
+                base.TabIndexChanged -= value;
+            }
         }
 
         [Browsable(false)]
@@ -202,12 +328,17 @@ namespace TerrainMapGUILibrary.Extensions
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new event EventHandler TextChanged
         {
-            add { base.TextChanged += value; }
-            remove { base.TextChanged -= value; }
+            add
+            {
+                base.TextChanged += value;
+            }
+            remove
+            {
+                base.TextChanged -= value;
+            }
         }
 
         public ControlExtension()
-            : base()
         {
             Font = FontTheme.Normal();
             Size = new Size(100, 100);
